328fa3bd6a0260cde1211f9a62ce78bfe6d7e075
nkihrk <shoma.hamada@gmail.com>
2020-12-24 20:14:25 +0900

    [add] added a new tailwindcss component to make tailwindcss inline

---
+++ apps/test-websocket/src/app/tailwindcss/tailwindcss.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+	selector: 'toybox-tailwindcss',
+	templateUrl: './tailwindcss.component.html',
+	styleUrls: ['./tailwindcss.component.scss'],
+	encapsulation: ViewEncapsulation.None
+})
+export class TailwindcssComponent {}
