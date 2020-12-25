cbeda9849c85194e459779ea36bf810097645c28
nkihrk <40584391+nkihrk@users.noreply.github.com>
2020-12-26 02:54:19 +0900

    [updated] updated views of chat and users. Refactored file structures

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
 	purge: ['./apps/**/*.html', './apps/**/*.ts', './apps/**/*.scss'],
 	darkMode: 'class', // or 'media' or 'class'
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'discord-light': '#36393F',
+				'discord-lightest': '#D4D5D6',
+				'discord-dark': '#2F3136',
+				'discord-darkest': '#202225'
+			}
+		}
 	},
 	variants: {
 		extend: {}
