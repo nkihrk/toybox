7cdbabe9d7af046370e0bba8b94b36e9dc154628
nkihrk <shoma.hamada@gmail.com>
2020-12-25 21:52:44 +0900

    [add] added Fontawesome

--- apps/chat-app/src/app/shared/shared.module.ts
+++ apps/chat-app/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuicklinkModule } from 'ngx-quicklink';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
 	declarations: [],
-	imports: [CommonModule, QuicklinkModule, FlexLayoutModule],
+	imports: [CommonModule, QuicklinkModule, FlexLayoutModule, FontAwesomeModule],
 	exports: [QuicklinkModule]
 })
 export class SharedModule {}
