9187957ed0ae74966655be4a5723d85b468c613e
nkihrk <40584391+nkihrk@users.noreply.github.com>
2020-12-25 22:29:58 +0900

    [update] updated module and packages for NgRx

--- apps/chat-app/src/app/app.module.ts
+++ apps/chat-app/src/app/app.module.ts
@@ -3,10 +3,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		StoreModule.forRoot(
+			{},
+			{
+				metaReducers: !environment.production ? [] : [],
+				runtimeChecks: {
+					strictActionImmutability: true,
+					strictStateImmutability: true
+				}
+			}
+		),
+		EffectsModule.forRoot([]),
+		!environment.production ? StoreDevtoolsModule.instrument() : []
+	],
 	providers: [],
 	bootstrap: [AppComponent]
 })
