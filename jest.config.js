fa313931372c49afa095c0756a378ffd4b622928
nkihrk <40584391+nkihrk@users.noreply.github.com>
2020-12-25 19:37:16 +0900

    [add] created files for client and api-server

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-	projects: ['<rootDir>/apps/test-websocket', '<rootDir>/apps/websocket-server']
+	projects: [
+		'<rootDir>/apps/test-websocket',
+		'<rootDir>/apps/websocket-server',
+		'<rootDir>/apps/chat-app',
+		'<rootDir>/apps/chat-app-server'
+	]
 };
