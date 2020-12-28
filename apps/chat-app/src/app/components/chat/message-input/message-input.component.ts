4882c3f77d3cfe41c00b102440edcee02ec5cc51
nkihrk <40584391+nkihrk@users.noreply.github.com>
2020-12-28 15:19:09 +0900

    [update] updated message input. Need to make it more smoother on line breaking

--- apps/chat-app/src/app/components/chat/message-input/message-input.component.ts
+++ apps/chat-app/src/app/components/chat/message-input/message-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -7,22 +7,46 @@ import { FormControl } from '@angular/forms';
 	styleUrls: ['./message-input.component.scss']
 })
 export class MessageInputComponent implements OnInit {
+	@ViewChild('messageInput', { static: true }) messageInputRef: ElementRef<HTMLTextAreaElement>;
+
 	messageForm = new FormControl();
+	isShiftKey = false;
 
 	constructor() {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this._autoGrowTextZone(this.messageInputRef.nativeElement);
+	}
 
-	onEnter($event: any): void {
+	onKeyUp($event: any): void {
+		this._autoGrowTextZone($event.target);
+
+		if ($event.keyCode === 16) this.isShiftKey = false;
 		if ($event.keyCode !== 13) return;
 
-		const message: string = this.messageForm.value;
+		if (this.isShiftKey) {
+		} else {
+			const message: string = this.messageForm.value;
 
-		// Clean up the messageForm
-		this._resetMessageForm();
+			// Clean up the messageForm
+			this._resetMessageForm();
+			this._autoGrowTextZone($event.target);
+		}
+	}
+
+	private _autoGrowTextZone($elem: HTMLElement): void {
+		$elem.style.height = '0px';
+		$elem.style.height = $elem.scrollHeight + 'px';
 	}
 
 	private _resetMessageForm(): void {
 		this.messageForm.reset();
+	}
+
+	onKeyDown($event: any): void {
+		if ($event.keyCode === 13) {
+		} else if ($event.keyCode == 16) {
+			this.isShiftKey = true;
+		}
 	}
 }
