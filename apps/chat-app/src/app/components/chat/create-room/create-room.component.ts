33064133664c70f7b0bd714dd31e1e3a928b5da0
nkihrk <shoma.hamada@gmail.com>
2020-12-29 19:38:29 +0900

    [update/chat-app] added a reactive form and validation for the room creation dialog

--- apps/chat-app/src/app/components/chat/create-room/create-room.component.ts
+++ apps/chat-app/src/app/components/chat/create-room/create-room.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { v4 as uuidv4, validate as uuidValidate } from 'uuid';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
 	selector: 'toybox-create-room',
@@ -12,6 +12,8 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 export class CreateRoomComponent implements OnInit, OnDestroy {
 	isCreateRoom = true;
 	createRoomForm: FormGroup;
+	isShowErrors = false;
+	isButtonEnabled = true;
 
 	private subscriptions: Subscription[] = [];
 
@@ -26,9 +28,43 @@ export class CreateRoomComponent implements OnInit, OnDestroy {
 		this.subscriptions.forEach(($subscription: Subscription) => $subscription.unsubscribe());
 	}
 
+	get roomNameControl(): AbstractControl {
+		return this.createRoomForm.get('roomName');
+	}
+
+	createRoom(): void {
+		if (this.createRoomForm.invalid) {
+			this.isShowErrors = true;
+			return;
+		}
+
+		// prevent multi-clicking
+		this.isButtonEnabled = false;
+
+		const roomId: string = uuidv4();
+		const roomName: string = this.createRoomForm.get('roomName').value;
+
+		this.router.navigate(['/room'], { queryParams: { id: roomId, name: roomName } }).then(
+			() => {
+				this._resetFlags();
+
+				// reset form
+				this.createRoomForm.get('roomName').setValue('');
+			},
+			($error: any) => {
+				this._resetFlags();
+			}
+		);
+	}
+
+	private _resetFlags(): void {
+		this.isButtonEnabled = true;
+		this.isShowErrors = false;
+	}
+
 	private _createForm(): void {
 		this.createRoomForm = this.builder.group({
-			'room-name': new FormControl('', [Validators.required])
+			roomName: new FormControl('', [Validators.required])
 		});
 	}
 
@@ -36,7 +72,7 @@ export class CreateRoomComponent implements OnInit, OnDestroy {
 		this.subscriptions.push(
 			this.route.queryParamMap.subscribe(
 				($params: ParamMap) => {
-					const roomId = $params.get('id');
+					const roomId: string = $params.get('id');
 
 					if (uuidValidate(roomId)) {
 						// disable room creation dialog
@@ -48,16 +84,8 @@ export class CreateRoomComponent implements OnInit, OnDestroy {
 				},
 				($error: any) => {
 					console.log($error);
-				},
-				() => {
-					console.log('done successfully');
 				}
 			)
 		);
-	}
-
-	private _createRoom(): void {
-		const roomId: string = uuidv4();
-		this.router.navigate(['/room'], { queryParams: { id: roomId } });
 	}
 }
