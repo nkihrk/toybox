550101d5c341cac32d37b9e80fea2d93573d29ec
nkihrk <40584391+nkihrk@users.noreply.github.com>
2021-01-08 18:59:41 +0900

    [update/workspace] removed projects related to test-websocket

--- apps/websocket-server/src/app/events/events.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EventsGateway } from './events.gateway';
-
-@Module({
-	imports: [],
-	controllers: [],
-	providers: [EventsGateway]
-})
-export class EventsModule {}
--- apps/websocket-server/src/controllers/controllers.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TestController } from './test/test.controller';
-
-@Module({
-  controllers: [TestController]
-})
-export class ControllersModule {}
