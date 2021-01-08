001029102e27fe4e974342a0545ddc5b9eca3883
nkihrk <40584391+nkihrk@users.noreply.github.com>
2021-01-08 21:30:24 +0900

    [add/chat-app-redis] created a redis-cache module and service

--- apps/chat-app-redis-server/src/app/app.module.ts
+++ apps/chat-app-redis-server/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GatewaysModule } from './gateways/gateways.module';
+import { RedisCacheModule } from './redis-cache/redis-cache.module';
 
 @Module({
-	imports: [GatewaysModule],
+	imports: [GatewaysModule, RedisCacheModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
