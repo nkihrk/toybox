d7bb07a599591f0ea84c9a7815b253a61854979b
nkihrk <40584391+nkihrk@users.noreply.github.com>
2021-01-09 14:37:52 +0900

    [update/chat-app-redis] linted

--- apps/chat-app-redis-server/src/app/redis-cache/services/redis-cache.service.spec.ts
+++ apps/chat-app-redis-server/src/app/redis-cache/services/redis-cache.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RedisCacheService } from './redis-cache.service';
 
 describe('RedisCacheService', () => {
-  let service: RedisCacheService;
+	let service: RedisCacheService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [RedisCacheService],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [RedisCacheService]
+		}).compile();
 
-    service = module.get<RedisCacheService>(RedisCacheService);
-  });
+		service = module.get<RedisCacheService>(RedisCacheService);
+	});
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
 });
