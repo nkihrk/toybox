24a73b88f8f03e85800fbcf8b1bb5e5bda7cb074
nkihrk <40584391+nkihrk@users.noreply.github.com>
2021-01-10 17:48:19 +0900

    [update/chat-app-redis] made it work dotenv

--- apps/chat-app-redis-server/src/app/redis-cache/redis-cache.module.ts
+++ apps/chat-app-redis-server/src/app/redis-cache/redis-cache.module.ts
@@ -1,13 +1,17 @@
 import { CacheModule, Module } from '@nestjs/common';
 import { RedisCacheService } from './services/redis-cache.service';
 import * as redisStore from 'cache-manager-redis-store';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
 	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true
+		}),
 		CacheModule.register({
 			store: redisStore,
-			host: 'localhost',
-			port: 6379
+			host: process.env.REDIS_HOST,
+			port: Number(process.env.REDIS_PORT)
 		})
 	],
 	providers: [RedisCacheService],
